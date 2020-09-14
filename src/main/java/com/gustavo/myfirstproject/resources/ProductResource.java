0c5f3dd47d48053721345645163cf44f2835f192
Gustavo Victorio <gu.victorio3@gmail.com>
2020-09-14 09:13:50 -0300

    Product memory

--- src/main/java/com/gustavo/myfirstproject/resources/ProductResource.java
+++ src/main/java/com/gustavo/myfirstproject/resources/ProductResource.java
@@ -9,25 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gustavo.myfirstproject.entities.Category;
-import com.gustavo.myfirstproject.repositories.CategoryRepository;
+import com.gustavo.myfirstproject.entities.Product;
+import com.gustavo.myfirstproject.repositories.ProductRepository;
 
 @RestController
-@RequestMapping(value = "/categories")
-public class CategoryResource {
+@RequestMapping(value = "/products")
+public class ProductResource {
 
 	@Autowired
-	private CategoryRepository categoryRepository;
+	private ProductRepository categoryRepository;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = categoryRepository.findAll();
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> list = categoryRepository.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Category> findById(@PathVariable Long id) {
-		Category cat = categoryRepository.findById(id);
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product cat = categoryRepository.findById(id);
 		return ResponseEntity.ok().body(cat);
 	}
 }
